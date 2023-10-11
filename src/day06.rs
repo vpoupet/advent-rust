f9d1b3355c8cb2754c3c1c22f1f8ca75eee9c703
vpoupet <victor.poupet@gmail.com>
2023-10-11 20:54:02 +0200

    feat: day 6

---
+++ src/day06.rs
@@ -0,0 +1,72 @@
+use crate::utils;
+
+pub fn solve1() -> i32 {
+    let input = utils::read_input("src/day06.txt").unwrap();
+    let mut buffer = Vec::new();
+    
+    let mut chars = input.chars();
+    let mut counter = 0;
+    for _ in 0..4 {
+        buffer.push(chars.next().unwrap());
+        counter += 1;
+    }
+
+    loop {
+        if buffer[0] != buffer[1] && buffer[0] != buffer[2] && buffer[0] != buffer[3]
+        && buffer[1] != buffer[2] && buffer[1] != buffer[3] && buffer[2] != buffer[3] {
+            return counter;
+        }
+        buffer.remove(0);
+        buffer.push(chars.next().unwrap());
+        counter += 1;
+    }
+}
+
+fn are_all_different(buffer: &Vec<char>) -> bool {
+    for i in 0..buffer.len() {
+        for j in i+1..buffer.len() {
+            if buffer[i] == buffer[j] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+pub fn solve2() -> i32 {
+    let input = utils::read_input("src/day06.txt").unwrap();
+    let mut buffer = Vec::new();
+    
+    let mut chars = input.chars();
+    let mut counter = 0;
+    for _ in 0..14 {
+        buffer.push(chars.next().unwrap());
+        counter += 1;
+    }
+
+    loop {
+        if are_all_different(&buffer) {
+            return counter;
+        }
+        buffer.remove(0);
+        buffer.push(chars.next().unwrap());
+        counter += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve1() {
+        println!("Part One: {}", solve1());
+        assert_eq!(solve1(), 1623);
+    }
+
+    #[test]
+    fn test_solve2() {
+        println!("Part Two: {}", solve2());
+        assert_eq!(solve2(), 3774);
+    }
+}
