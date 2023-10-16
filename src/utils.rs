bd0deb012a393d1f6cd7c4d8dd0c49ca27f82153
vpoupet <victor.poupet@gmail.com>
2023-10-16 09:51:42 +0200

    feat: day 15

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,28 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+use nom::IResult;
+use nom::character::complete::{char, digit1};
+use nom::combinator::{map, opt};
+use nom::sequence::pair;
+
 pub fn read_input(filename: &str) -> Result<String, std::io::Error> {
     // open file given as input and returns its content as a String
     let mut file = File::open(filename)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     Ok(contents)
+}
+
+pub fn parse_signed_int(input: &str) -> IResult<&str, i32> {
+    map(
+        pair(opt(char('-')), digit1),
+        |(sign, number): (Option<char>, &str)| {
+            let value = number.parse::<i32>().unwrap();
+            if sign == Some('-') {
+                return -value;
+            }
+            return value;
+        },
+    )(input)
 }
