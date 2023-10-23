bd55e95b988018e836ffa7f9221ee464f10cc7e9
Victor Poupet <victor.poupet@gmail.com>
2023-10-23 22:23:33 +0200

    feat: day 20

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -1,32 +1,71 @@
 use crate::utils;
 
-pub fn solve1() -> i32 {
-    let input = utils::read_input("src/day20/example.txt").unwrap();
-    let mut values = Vec::new();
+fn mix(mut values: Vec<i64>, nb_times: usize) -> Vec<i64> {
+    let n = values.len();
+    // indexes[i] is the current index in values of the value that was initially at index i
+    let mut indexes = (0..n).collect::<Vec<usize>>();
 
-    for line in input.lines() {
-        values.push(line.parse::<i32>().unwrap());
-    }
-    let n = values.len() as i32;
+    for _ in 0..nb_times {
+        for i in 0..n {
+            let start_index = indexes[i];
+            let value = values[start_index];
+            values.remove(start_index);
+            let end_index = (start_index as i64 + value).rem_euclid(n as i64 - 1) as usize;
+            values.insert(end_index, value);
 
-    println!("{:?}", values);
-    for value in values.clone().iter() {
-        let index = values.iter().position(|&x| x == *value).unwrap();
-        values.remove(index);
-        let new_index = ((index as i32 + *value + n) % n) as usize;
-        if new_index > index {
-            values.insert(new_index + 1, *value);
-        } else {
-            values.insert(new_index, *value);
+            if start_index < end_index {
+                for j in 0..n {
+                    if start_index < indexes[j] && indexes[j] <= end_index {
+                        indexes[j] -= 1;
+                    }
+                }
+            } else if end_index < start_index {
+                for j in 0..n {
+                    if end_index <= indexes[j] && indexes[j] < start_index {
+                        indexes[j] += 1;
+                    }
+                }
+            }
+
+            indexes[i] = end_index;
         }
-        println!("{:?}", values);
     }
-    0
+
+    values
 }
 
-pub fn solve2() -> i32 {
-    let input = utils::read_input("src/dayXX/input.txt").unwrap();
-    0
+pub fn solve1() -> i64 {
+    let input = utils::read_input("src/day20/input.txt").unwrap();
+    let mut values = Vec::new();
+    for line in input.lines() {
+        values.push(line.parse::<i64>().unwrap());
+    }
+
+    let values = mix(values, 1);
+    let n = values.len();
+
+    let zero_index = values.iter().position(|&x| x == 0).unwrap();
+
+    values[(zero_index + 1000) % n]
+        + values[(zero_index + 2000) % n]
+        + values[(zero_index + 3000) % n]
+}
+
+pub fn solve2() -> i64 {
+    let input = utils::read_input("src/day20/input.txt").unwrap();
+    let mut values = Vec::new();
+    for line in input.lines() {
+        values.push(line.parse::<i64>().unwrap() * 811589153);
+    }
+    
+    let values = mix(values, 10);
+    let n = values.len();
+
+    let zero_index = values.iter().position(|&x| x == 0).unwrap();
+
+    values[(zero_index + 1000) % n]
+        + values[(zero_index + 2000) % n]
+        + values[(zero_index + 3000) % n]
 }
 
 #[cfg(test)]
@@ -37,13 +76,13 @@ mod tests {
     fn test_solve1() {
         let solution = solve1();
         println!("Part One: {}", solution);
-        // assert_eq!(solution, 0);
+        assert_eq!(solution, 13967);
     }
 
     #[test]
     fn test_solve2() {
         let solution = solve2();
         println!("Part Two: {}", solution);
-        // assert_eq!(solution, 0);
+        assert_eq!(solution, 1790365671518);
     }
 }
