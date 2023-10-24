be2e695b06a46c405bd01dd2c2033779fcb52429
vpoupet <victor.poupet@gmail.com>
2023-10-24 19:26:28 +0200

    Prepared setup for days 24 and 25

---
+++ src/day25/mod.rs
@@ -0,0 +1,30 @@
+use crate::utils;
+
+pub fn solve1() -> i32 {
+    let _input = utils::read_input("src/day25/input.txt").unwrap();
+    0
+}
+
+pub fn solve2() -> i32 {
+    let _input = utils::read_input("src/day25/input.txt").unwrap();
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve1() {
+        let solution = solve1();
+        println!("Part One: {}", solution);
+        // assert_eq!(solution, 0);
+    }
+
+    #[test]
+    fn test_solve2() {
+        let solution = solve2();
+        println!("Part Two: {}", solution);
+        // assert_eq!(solution, 0);
+    }
+}
