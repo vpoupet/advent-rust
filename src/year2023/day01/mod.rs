3ddc8d01a89a528afab2a866aaa9661d00ad803a
Victor Poupet <victor.poupet@gmail.com>
2023-12-01 21:20:23 +0100

    refactor: day 1

--- src/year2023/day01/mod.rs
+++ src/year2023/day01/mod.rs
@@ -1,30 +1,6 @@
-use nom::{branch::alt, bytes::complete::tag, combinator::map, IResult};
+use std::collections::HashMap;
 
 use crate::utils;
-
-fn parse_digit(input: &str) -> IResult<&str, i32> {
-    alt((
-        map(tag("one"), |_| 1),
-        map(tag("two"), |_| 2),
-        map(tag("three"), |_| 3),
-        map(tag("four"), |_| 4),
-        map(tag("five"), |_| 5),
-        map(tag("six"), |_| 6),
-        map(tag("seven"), |_| 7),
-        map(tag("eight"), |_| 8),
-        map(tag("nine"), |_| 9),
-        map(tag("0"), |_| 0),
-        map(tag("1"), |_| 1),
-        map(tag("2"), |_| 2),
-        map(tag("3"), |_| 3),
-        map(tag("4"), |_| 4),
-        map(tag("5"), |_| 5),
-        map(tag("6"), |_| 6),
-        map(tag("7"), |_| 7),
-        map(tag("8"), |_| 8),
-        map(tag("9"), |_| 9),
-    ))(input)
-}
 
 pub fn solve1() -> i32 {
     let input = utils::read_input("src/year2023/day01/input.txt").unwrap();
@@ -51,16 +27,44 @@ pub fn solve1() -> i32 {
 pub fn solve2() -> i32 {
     let input = utils::read_input("src/year2023/day01/input.txt").unwrap();
     let mut total = 0;
+
+    let digits: HashMap<_, _> = [
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
     for line in input.lines() {
         let mut c1: Option<i32> = None;
         let mut c2: Option<i32> = None;
         for i in 0..line.len() {
-            let p = parse_digit(&line[i..]);
-            if let Ok((_, digit)) = p {
-                if c1.is_none() {
-                    c1 = Some(digit);
+            for key in digits.keys() {
+                if line[i..].starts_with(*key) {
+                    if c1.is_none() {
+                        c1 = Some(digits[key]);
+                    }
+                    c2 = Some(digits[key]);
+                    break;
                 }
-                c2 = Some(digit);
             }
         }
         if let (Some(c1), Some(c2)) = (c1, c2) {
