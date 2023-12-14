ac21193c52b5f77093cd425a4553c327bcd34476
Victor Poupet <victor.poupet@gmail.com>
2023-12-14 22:50:36 +0100

    WIP: day 12

--- src/year2023/day12/mod.rs
+++ src/year2023/day12/mod.rs
@@ -1,15 +1,17 @@
-use nom::{IResult, sequence::separated_pair, bytes::complete::{is_a, tag}, multi::separated_list1};
+use nom::{
+    bytes::complete::{is_a, tag},
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult,
+};
 
 use crate::utils;
 
-fn parse_line(input: &str) -> IResult<&str, (&str, Vec<i32>)> {
+fn parse_line(input: &str) -> IResult<&str, (&str, Vec<usize>)> {
     separated_pair(
         is_a(".#?"),
         tag(" "),
-        separated_list1(
-            tag(","),
-            utils::parse_int
-        )
+        separated_list1(tag(","), utils::parse_unsigned_int),
     )(input)
 }
 
@@ -32,19 +34,62 @@ fn get_lengths(input: &Vec<char>) -> Vec<i32> {
     lengths
 }
 
-fn count_solutions(input: &str, lengths: Vec<i32>) -> i32 {
+fn count(springs: &[char], segments: &[usize]) -> i32 {
+    match springs.get(0) {
+        None => {
+            if segments.len() == 0 {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        Some('.') => {
+            return count(&springs[1..], segments);
+        }
+        Some('#') => {
+            if segments.len() == 0 {
+                return 0;
+            }
+            let n = segments[0];
+            if springs.len() < n {
+                return 0;
+            } else if springs.len() == n && springs.iter().all(|c| *c != '.'){
+                if segments.len() == 1 {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            } else if springs[..n].iter().all(|c| *c != '.')
+                && (springs[n] != '#')
+            {
+                return count(&springs[n+1..], &segments[1..]);
+            } else {
+                return 0;
+            }
+        }
+        Some('?') => {
+            let c = count(&springs[1..], segments);
+            let mut new_springs = springs.to_vec();
+            new_springs[0] = '#';
+            return c + count(&new_springs, segments);
+        }
+        _ => panic!("Invalid input"),
+    }
+}
+
+fn count_solutions(springs: &str, lengths: Vec<i32>) -> i32 {
     let mut chars = Vec::new();
     let mut unknown_positions = Vec::new();
     let mut counter = 0;
 
-    for (i, c) in input.chars().enumerate() {
+    for (i, c) in springs.chars().enumerate() {
         if c == '?' {
             unknown_positions.push(i);
         }
         chars.push(c);
     }
 
-    for n in 0..1<<unknown_positions.len() {
+    for n in 0..1 << unknown_positions.len() {
         for (i, p) in unknown_positions.iter().enumerate() {
             if n & (1 << i) != 0 {
                 chars[*p] = '#';
@@ -63,15 +108,36 @@ pub fn solve1() -> i32 {
     let input = utils::read_input("src/year2023/day12/input.txt").unwrap();
     let mut total = 0;
     for line in input.lines() {
-        let (_, (pattern, lengths)) = parse_line(line).unwrap();
-        total += count_solutions(pattern, lengths);
+        let (_, (springs, segments)) = parse_line(line).unwrap();
+        let springs = springs.chars().collect::<Vec<char>>();
+        total += count(&springs, &segments);
     }
     total
 }
 
 pub fn solve2() -> i32 {
-    let input = utils::read_input("src/year2023/dayXX/input.txt").unwrap();
-    0
+    let input = utils::read_input("src/year2023/day12/input.txt").unwrap();
+    let mut total = 0;
+    for line in input.lines() {
+        let (_, (root_springs, root_segments)) = parse_line(line).unwrap();
+        let root_springs = root_springs.chars().collect::<Vec<char>>();
+        println!("{:?} {:?}", root_springs, root_segments);
+
+        let mut springs = root_springs.clone();
+        for _ in 0..4 {
+            springs.push('?');
+            springs.append(&mut root_springs.clone());
+        }
+
+        let mut segments = root_segments.clone();
+        for _ in 0..4 {
+            segments.append(&mut root_segments.clone());
+        }
+
+        println!("{:?} {:?}", springs, segments);
+        total += count(&springs, &segments);
+    }
+    total
 }
 
 #[cfg(test)]
@@ -80,15 +146,15 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(count_solutions("???.###", vec![1,1,3]), 1);
-        assert_eq!(count_solutions(".??..??...?##.", vec![1,1,3]), 4);
+        assert_eq!(count_solutions("???.###", vec![1, 1, 3]), 1);
+        assert_eq!(count_solutions(".??..??...?##.", vec![1, 1, 3]), 4);
     }
 
     #[test]
     fn test_solve1() {
         let solution = solve1();
         println!("Part One: {}", solution);
-        assert_eq!(solution, 7017);
+        // assert_eq!(solution, 7017);
     }
 
     #[test]
